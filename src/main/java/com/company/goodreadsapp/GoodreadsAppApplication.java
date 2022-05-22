77fd73e8c71c5f5c7b902cb943b570a59a13d351
ibrahimx012 <ibrahimreshid7@gmail.com>
2022-05-22 18:26:50 +0400

    Create Database with liquibase

---
+++ src/main/java/com/company/goodreadsapp/GoodreadsAppApplication.java
@@ -0,0 +1,13 @@
+package com.company.goodreadsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodreadsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodreadsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/goodreadsapp/GoodreadsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.goodreadsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodreadsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
