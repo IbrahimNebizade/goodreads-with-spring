e4c9e79486ce8373444332c11e38f7ed019f817f
ibrahimx012 <ibrahimreshid7@gmail.com>
2022-05-22 18:47:24 +0400

    Create model class

---
+++ src/main/java/com/company/goodreadsapp/model/BaseModel.java
@@ -0,0 +1,15 @@
+package com.company.goodreadsapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseModel {
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdatedAt;
+}
