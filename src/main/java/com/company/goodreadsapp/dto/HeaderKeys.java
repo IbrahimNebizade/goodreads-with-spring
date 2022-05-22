7531c3d67c6a506790e41530198f1ade12fed9b9
ibrahimx012 <ibrahimreshid7@gmail.com>
2022-05-22 19:00:05 +0400

    Create DTO class

---
+++ src/main/java/com/company/goodreadsapp/dto/HeaderKeys.java
@@ -0,0 +1,7 @@
+package com.company.goodreadsapp.dto;
+
+public interface HeaderKeys {
+    String ACCESS_TOKEN = "x-access-token";
+    String REFRESH_TOKEN = "x-refresh-token";
+    String USER_ID = "x-user-id";
+}
